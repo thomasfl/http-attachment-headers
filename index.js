8cd092abc20998083cd50eabbc4f685f1c384477
thomasf <thomas.flemming@gmail.com>
2015-06-09 10:27:18 +0200

    Only detect IE9 and IE10

--- index.js
+++ index.js
@@ -9,29 +9,35 @@ var fs = require('fs');
 
 app.get('/', function (req, res) {
     var ua = req.headers['user-agent'];
-    var internetExplorer = isInternetExplorer(req);
+    var IE9orIE10 = isIE9orIE10(req);
     res.send('<html><head>' +
              '<title>PDF download</title></head><body>' +
              /* The download attribute make browsers download
               instead of displaying file. Not supported in IE.
               download="example.pdf" */
              '<p><a href="example.pdf">Download PDF</a></p>' +
-             '<p>Internet Explorer: ' + internetExplorer + '</p>' +
+             '<p>IE9 or IE10: ' + IE9orIE10 + '</p>' +
              '<p>User agent: ' + ua + '</p>' +
              '</body></html>');
 });
 
-function isInternetExplorer(request) {
+/* isIE9orIE10() User agent check for:
+ * IE9:
+ *   Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.0; Trident/5.0)
+ * IE10:
+ *   Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.1; WOW64; Trident/6.0)
+ */
+function isIE9orIE10(request) {
     var ua = request.headers['user-agent'];
-    return (/MSIE/.test(ua) || /Trident/.test(ua));
-};
+    return (/MSIE 9\.0/.test(ua) || /MSIE 10\.0/.test(ua));
+}
 
 app.get('/example.pdf', function (req, res) {
     var ua = req.headers['user-agent'];
     var headers = {
         'Content-type': 'application/pdf'
     };
-    if(isInternetExplorer(req)) {
+    if(isIE9orIE10(req)) {
         headers = {
             /* Make browsers download instead of displaying file:
              http://en.wikipedia.org/wiki/MIME#Content-Disposition */
